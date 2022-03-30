a48242ff599985be77f9c721ba460d07a0876033
Cheng Yang <yangche@umich.edu>
2022-03-30 18:18:03 -0400

    updates

--- src/SUMO_utils.py
+++ src/SUMO_utils.py
@@ -9,6 +9,7 @@ import os
 import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt
+import matplotlib
 
 class SUMO_utils():
     """
@@ -310,3 +311,162 @@ class SUMO_utils():
         y = self.dyn_dic[sheet_name][y_name]
         line = ax.scatter(x,y,**kwargs)
         return line
+    
+    def heatmap(self, data, row_labels, col_labels, ax,
+            cbar_kw={}, cbarlabel="", **kwargs):
+        """
+        Create a heatmap from a numpy array and two lists of labels.
+    
+        Parameters
+        ----------
+        data
+            A 2D numpy array of shape (N, M).
+        row_labels
+            A list or array of length N with the labels for the rows.
+        col_labels
+            A list or array of length M with the labels for the columns.
+        ax
+            A `matplotlib.axes.Axes` instance to which the heatmap is plotted.  If
+            not provided, use current axes or create a new one.  Optional.
+        cbar_kw
+            A dictionary with arguments to `matplotlib.Figure.colorbar`.  Optional.
+        cbarlabel
+            The label for the colorbar.  Optional.
+        **kwargs
+            All other arguments are forwarded to `imshow`.
+        """
+
+    
+        # Plot the heatmap
+        im = ax.imshow(data, **kwargs)
+    
+        # Create colorbar
+        cbar = ax.figure.colorbar(im, ax=ax, **cbar_kw)
+        cbar.ax.set_ylabel(cbarlabel, rotation=-90, va="bottom")
+    
+        # We want to show all ticks...
+        ax.set_xticks(np.arange(data.shape[1]))
+        ax.set_yticks(np.arange(data.shape[0]))
+        # ... and label them with the respective list entries.
+        ax.set_xticklabels(col_labels)
+        ax.set_yticklabels(row_labels)
+    
+        # Let the horizontal axes labeling appear on top.
+        ax.tick_params(top=True, bottom=False,
+                       labeltop=True, labelbottom=False)
+    
+        # Rotate the tick labels and set their alignment.
+        plt.setp(ax.get_xticklabels(), rotation=-30, ha="right",
+                 rotation_mode="anchor")
+    
+        # Turn spines off and create white grid.
+        # ax.spines[:].set_visible(False)
+    
+        ax.set_xticks(np.arange(data.shape[1]+1)-.5, minor=True)
+        ax.set_yticks(np.arange(data.shape[0]+1)-.5, minor=True)
+        ax.grid(which="minor", color="w", linestyle='-', linewidth=3)
+        ax.tick_params(which="minor", bottom=False, left=False)
+    
+        return im, cbar
+
+
+    def annotate_heatmap(self, im, data=None, valfmt="{x:.2f}",
+                         textcolors=("black", "white"),
+                         threshold=None, **textkw):
+        """
+        A function to annotate a heatmap.
+    
+        Parameters
+        ----------
+        im
+            The AxesImage to be labeled.
+        data
+            Data used to annotate.  If None, the image's data is used.  Optional.
+        valfmt
+            The format of the annotations inside the heatmap.  This should either
+            use the string format method, e.g. "$ {x:.2f}", or be a
+            `matplotlib.ticker.Formatter`.  Optional.
+        textcolors
+            A pair of colors.  The first is used for values below a threshold,
+            the second for those above.  Optional.
+        threshold
+            Value in data units according to which the colors from textcolors are
+            applied.  If None (the default) uses the middle of the colormap as
+            separation.  Optional.
+        **kwargs
+            All other arguments are forwarded to each call to `text` used to create
+            the text labels.
+        """
+    
+        if not isinstance(data, (list, np.ndarray)):
+            data = im.get_array()
+    
+        # Normalize the threshold to the images color range.
+        if threshold is not None:
+            threshold = im.norm(threshold)
+        else:
+            threshold = im.norm(data.max())/2.
+    
+        # Set default alignment to center, but allow it to be
+        # overwritten by textkw.
+        kw = dict(horizontalalignment="center",
+                  verticalalignment="center")
+        kw.update(textkw)
+    
+        # Get the formatter in case a string is supplied
+        if isinstance(valfmt, str):
+            valfmt = matplotlib.ticker.StrMethodFormatter(valfmt)
+    
+        # Loop over the data and create a `Text` for each "pixel".
+        # Change the text's color depending on the data.
+        texts = []
+        for i in range(data.shape[0]):
+            for j in range(data.shape[1]):
+                kw.update(color=textcolors[int(im.norm(data[i, j]) > threshold)])
+                text = im.axes.text(j, i, valfmt(data[i, j], None), **kw)
+                texts.append(text)
+    
+        return texts
+    
+    def create_heatmap(self, my_df, x_name, y_name, z_name, ax, cbarlabel = None, cmap="YlGn",valfmt="{x:.3f}"): 
+        """
+        Create 2D heatmaps to visual 
+
+        Parameters
+        ----------
+        my_df : TYPE
+            DESCRIPTION.
+        x_name : TYPE
+            DESCRIPTION.
+        y_name : TYPE
+            DESCRIPTION.
+        z_name : TYPE
+            DESCRIPTION.
+        ax : TYPE
+            DESCRIPTION.
+        cbarlabel : TYPE, optional
+            DESCRIPTION. The default is None.
+        cmap : TYPE, optional
+            DESCRIPTION. The default is "YlGn".
+        valfmt : TYPE, optional
+            DESCRIPTION. The default is "{x:.3f}".
+
+        Returns
+        -------
+        ax : TYPE
+            DESCRIPTION.
+
+        """
+        targetmapdata = my_df.pivot_table(index = y_name, columns = x_name, values = z_name)
+        Z = targetmapdata.to_numpy()
+        x_ticks = np.unique(my_df[x_name])
+        y_ticks = np.unique(my_df[y_name])
+        # return Z, x_ticks, y_ticks
+        if cbarlabel == None:
+            cbarlabel = z_name
+        im, cbar = self.heatmap(Z, col_labels=x_ticks, row_labels=y_ticks,ax = ax, cmap= cmap, cbarlabel= cbarlabel)
+        self.annotate_heatmap(im,Z,valfmt=valfmt)
+        ax.set_xlabel(x_name)
+        ax.set_ylabel(y_name)
+        
+        return ax
